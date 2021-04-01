53f6160db9e8c1f9a2eb12a11b55ec8a66d8bb87
marcoshenriquemaia <henriquemarcosmaia@gmail.com>
2021-04-01 18:37:30 -0300

    change

---
+++ myScript/index.js
@@ -0,0 +1,23 @@
+//leia o README
+
+const marcosScript = (scenery, myMove) => {
+  // script teste
+  let moveIndex = null
+  const move = () => {
+    const randomNumber = Math.floor(Math.random() * 9)
+    
+    if (scenery[randomNumber]) return move()
+
+    moveIndex = randomNumber
+  }
+
+  move()
+  //script teste
+
+  // your script here
+
+  return moveIndex
+}
+
+
+export default marcosScript
